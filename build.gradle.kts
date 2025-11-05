fa12d0a20ad279bb43708f57f624b2d690ff6efd
Long Yang Late <110956382+longsydn1105@users.noreply.github.com>
2025-11-05 09:45:25 +0700

    feat: setup exposed (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,16 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+
+    implementation("org.jetbrains.exposed:exposed-core:0.41.1") // (Check version mới nhất)
+
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.41.1")
+
+    implementation("org.postgresql:postgresql:42.5.0")
+
+    implementation("com.zaxxer:HikariCP:5.0.1")
 }
 
 ktlint {
