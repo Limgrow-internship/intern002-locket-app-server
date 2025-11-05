adda5119e1b7f5e2a9c7c0c67713ce70536e02cc
Long Dai Ca <longsydn1105@gmail.com>
2025-11-05 15:25:54 +0700

    feat: add users and fcm_tokens tables schema

--- src/main/kotlin/com/intern002/locketapp/core/database/tables/FcmTokensTable.kt
+++ src/main/kotlin/com/intern002/locketapp/core/database/tables/FcmTokensTable.kt
@@ -1,2 +1,23 @@
 package com.intern002.locketapp.core.database.tables
 
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.kotlin.datetime.CurrentTimestamp
+import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
+
+object FcmTokensTable: Table("fcm_tokens") {
+
+    val id = uuid("id").autoGenerate()
+    val userId = uuid("user_id").references(UsersTable.id, onDelete = ReferenceOption.CASCADE)
+    val token = text("text")
+    val deviceInfo= text("device_info").nullable()
+    val isActive = bool("is_active").default(true)
+    val createdAt = timestamp("created_at").defaultExpression(CurrentTimestamp())
+    val updatedAt = timestamp("updated_at").defaultExpression(CurrentTimestamp())
+
+    override val primaryKey = PrimaryKey(id)
+
+    init {
+        uniqueIndex("fcm_tokens_user_id_token_unique", userId, token)
+    }
+}
--- src/main/kotlin/com/intern002/locketapp/core/security/TokenProvider.kt
+++ src/main/kotlin/com/intern002/locketapp/core/security/TokenProvider.kt
@@ -1,2 +1,1 @@
 package com.intern002.locketapp.core.security
-
