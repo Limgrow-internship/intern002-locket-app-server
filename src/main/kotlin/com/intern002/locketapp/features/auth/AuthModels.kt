89966d39b3f23fea4e079e373b5772dc1976dbf0
Long Dai Ca <longsydn1105@gmail.com>
2025-11-09 20:39:16 +0700

    feat(auth): add register dtos

--- src/main/kotlin/com/intern002/locketapp/features/auth/AuthModels.kt
+++ src/main/kotlin/com/intern002/locketapp/features/auth/AuthModels.kt
@@ -1,1 +1,27 @@
 package com.intern002.locketapp.features.auth
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthRequest(
+    val email: String,
+    val password: String,
+    val username: String,
+    val discriminator: Int,
+    val birthday: String
+)
+
+@Serializable
+data class AuthResponse(
+    val token: String,
+    val user: UserResponse
+)
+
+@Serializable
+data class UserResponse(
+    val id: String,
+    val email: String,
+    val username: String,
+    val discriminator: Int,
+    val birthday: String
+)
