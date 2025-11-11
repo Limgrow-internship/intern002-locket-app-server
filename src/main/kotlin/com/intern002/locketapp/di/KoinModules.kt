56eaca7e7c2e4680e0cbed6e5ea37607b7d5d2c0
Long Dai Ca <longsydn1105@gmail.com>
2025-11-11 22:46:37 +0700

    feat(auth): add register logic to service

--- src/main/kotlin/com/intern002/locketapp/di/KoinModules.kt
+++ src/main/kotlin/com/intern002/locketapp/di/KoinModules.kt
@@ -1,11 +1,13 @@
 package com.intern002.locketapp.di
 
 import com.intern002.locketapp.core.security.JwtTokenProvider
+import com.intern002.locketapp.core.security.TokenConfig
 import com.intern002.locketapp.core.security.TokenProvider
 import com.intern002.locketapp.core.utils.BcryptHashing
 import com.intern002.locketapp.core.utils.Hashing
 import com.intern002.locketapp.features.auth.AuthRepository
 import com.intern002.locketapp.features.auth.AuthService
+import io.ktor.server.application.Application
 import org.koin.dsl.module
 
 val appModule = module {
@@ -15,10 +17,20 @@ val appModule = module {
     single { AuthRepository() }
 
     single {
+        val config = get<Application>().environment.config
+        TokenConfig(
+            issuer = config.property("jwt.domain").getString(),
+            audience = config.property("jwt.audience").getString(),
+            expiresIn = config.property("expiresIn").getString().toLong(),
+            secret = config.property("jwt.secret").getString(),
+        )
+    }
+    single {
         AuthService(
             authRepository = get(),
             tokenProvider = get(),
-            hashing = get()
+            hashing = get(),
+            tokenConfig = get()
         )
     }
 }
--- src/main/kotlin/com/intern002/locketapp/features/auth/AuthService.kt
+++ src/main/kotlin/com/intern002/locketapp/features/auth/AuthService.kt
@@ -1,12 +1,40 @@
 package com.intern002.locketapp.features.auth
 
+import com.intern002.locketapp.core.security.TokenClaim
+import com.intern002.locketapp.core.security.TokenConfig
 import com.intern002.locketapp.core.security.TokenProvider
 import com.intern002.locketapp.core.utils.Hashing
 
 class AuthService(
     val authRepository: AuthRepository,
     val tokenProvider: TokenProvider,
-    val hashing: Hashing
+    val hashing: Hashing,
+    val tokenConfig: TokenConfig
 ) {
+    suspend fun registerUser(request: AuthRequest): AuthResponse {
+        val existingUser = authRepository.findUserByEmail(request.email)
+        if (existingUser != null) {
+            throw Exception("User with this email already exists")
+        }
+        val passwordHash = hashing.hash(request.password)
 
+        val newUser = authRepository.createUser(request, passwordHash)
+            ?: throw Exception("Fail to create user")
+
+        val token = tokenProvider.generateToken(
+            config = tokenConfig,
+            TokenClaim(name = "uid", value = newUser.id.toString())
+        )
+
+        return AuthResponse(
+            token = token,
+            user = UserResponse(
+                id = newUser.id.toString(),
+                email = newUser.email,
+                username = newUser.username,
+                discriminator = newUser.discriminator,
+                birthday = newUser.birthday.toString()
+            )
+        )
+    }
 }
